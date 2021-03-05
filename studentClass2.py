7ef7027fdaa10195475c2d78c4e42d3224a5b70b
Zhongqi Ma <mzhongqi@umich.edu>
2021-03-05 22:09:48 +0800

    Kevin

---
+++ studentClass2.py
@@ -0,0 +1,14 @@
+import numpy as np
+import random
+
+class Student():
+    def _init_(self, n_questions, theta_f=None, c_number):
+        self.n_questions = n_questions
+        self.theta_f = theta_f
+        self.c_number = c_number
+        
+    generate_theta(self):
+       if self.theta_f:
+            not none, pass
+        else:
+            tuple_Sskills = [[(random.random()) for i in range (c_number)] for i in range (c_number)]
