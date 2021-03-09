7f819c6b4df716c32c054b5b7c6a716b4bd6ef7f
Zhongqi Ma <59981957+mzhongqi@users.noreply.github.com>
2021-03-09 14:36:20 +0800

    Update studentClass2.py

--- studentClass2.py
+++ studentClass2.py
@@ -11,3 +11,21 @@ class Student():
             not none, pass
         else:
             self.theta_dict = {i:np.random.uniform(self.c_number) for i in range (self.c_number)}
+            
+    def generateAnswers(self):
+    i = 1
+    j = 1
+    k = 1
+    answers_3d = []
+    for i in range(self.n_questions - 1):
+        answers_2d = []
+        for j in range(self.c_number):
+            answers_1d = []
+            for k in range(self.c_number):
+                x = random.randint(0,1)
+                answers_1d.append(x)
+            else:
+                answers_2d.append(answers_1d)
+        else:
+            answers_3d.append(answers_2d)
+    return answers_3d
